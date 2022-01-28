4ced834ae438ae183253a85b56a4159a8edd21e7
N Tessa Pierce <ntpierce@gmail.com>
2022-01-27 19:07:00 -0800

    upd agg

--- conf/scripts/aggregate-pyani-results.py
+++ conf/scripts/aggregate-pyani-results.py
@@ -22,8 +22,23 @@ def main(args):
         select_path = args.path_name
         pathInfo = pathInfo[pathInfo["paths"] == select_path] # subset
     allpaths = pathInfo['paths'].unique().tolist()
+
+    #argh, passing in diff classes/labels isn't working. just convert here
+    acc2label = {}
+    if args.labels:
+        with open(args.labels, 'r') as lb:
+            for line in lb:
+                md5,fn,label=line
+                acc = fn.rsplit('_')[0]
+                acc2label[acc] = label
+    else:
+        acc2label[acc] = acc
+
+    print(acc2label)
+
     for path in allpaths:
         anchor_acc = pathInfo.loc[(pathInfo["path"] == path) & (pathInfo["rank"] == "anchor")]["accession"].values[0]
+        anchor_label = acc2label[anchor_acc]
         compare_accs = pathInfo.loc[(pathInfo["path"] == path) & (pathInfo["rank"] != "anchor")]["accession"].values.tolist()
 
         path_results_dir = os.path.join(args.pyani_results_dir, path, results)
@@ -43,14 +58,15 @@ def main(args):
         # get info for each comparison
         for rank in compare_ranklist:
             compare_acc = pathInfo.loc[(pathInfo["path"] == path) & (pathInfo["rank"] == rank)]["accession"].values[0]
+            compare_label = acc2label[compare_acc]
             comparison_name = f"{anchor_acc}_x_{compare_acc}"
             pyani_ident, pyani_coverage, pyani_aln_length, pyani_sim_errors, pyani_hadamard = np.nan, np.nan, np.nan, np.nan, np.nan
             # pyani SHOULD produce symmetrical matrix, so all accs should exist regardless of value, right?
-            pyani_ident = idD.at[anchor_acc, compare_acc]
-            pyani_coverage = covD.at[anchor_acc, compare_acc]
-            pyani_aln_length = lenD.at[anchor_acc, compare_acc]
-            pyani_sim_errors = seD.at[anchor_acc, compare_acc]
-            pyani_hadamard = hadD.at[anchor_acc, compare_acc]
+            pyani_ident = idD.at[anchor_label, compare_label]
+            pyani_coverage = covD.at[anchor_label, compare_label]
+            pyani_aln_length = lenD.at[anchor_label, compare_label]
+            pyani_sim_errors = seD.at[anchor_label, compare_label]
+            pyani_hadamard = hadD.at[anchor_label, compare_label]
 
             this_info = anchorpyani(comparison_name, anchor_acc, compare_acc, path, rank, pyani_ident, pyani_coverage, pyani_aln_length, pyani_sim_errors, pyani_hadamard)
             anchor_results.append(this_info)
@@ -68,6 +84,7 @@ def cmdline(sys_args):
     p.add_argument("pyani_results_dir")
     p.add_argument("--path-info", default="gtdb-r95-reps.pathinfo.tsv")
     p.add_argument("--path-name") # just do the one path
+    p.add_argument("--labels")
     p.add_argument("--output-csv", required=True)
     args = p.parse_args()
     return main(args)
